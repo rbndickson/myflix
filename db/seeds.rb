63ddb49c892357f8a4db63f394ce222806793d36
Robin Dickson <robindickson@gmail.com>
2015-09-03 15:10:49 +0200

    Updated seed to include categories

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,6 +6,11 @@
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
 
-Video.create(title: "Avatar - The Last Airbender", description: "The Avatar must master all 4 elements on his quest to save the world", small_cover_url:"avatar.jpg", large_cover_url: "avatar_large.jpg")
-Video.create(title: "The Office", description: "Comedy about British office life", small_cover_url:"office.jpg", large_cover_url: "office_large.jpg")
-Video.create(title: "The Amazing Race", description: "Contestants race around the world completing various challenges", small_cover_url:"amazing_race.jpg", large_cover_url: "amazing_race_large.jpg")
+comedy = Category.create(name: "Comedy")
+reality = Category.create(name: "Reality")
+animation = Category.create(name: "Animation")
+
+Video.create(title: "The Office", description: "Comedy about British office life", small_cover_url:"/tmp/office.jpg", large_cover_url: "/tmp/office_large.jpg", category_id: comedy.id)
+Video.create(title: "The Amazing Race", description: "Contestants race around the world completing various challenges", small_cover_url:"/tmp/amazing_race.jpg", large_cover_url: "/tmp/amazing_race_large.jpg", category_id: reality.id)
+Video.create(title: "Avatar - The Last Airbender", description: "The Avatar must master all 4 elements on his quest to save the world", small_cover_url:"/tmp/avatar.jpg", large_cover_url: "/tmp/avatar_large.jpg", category_id: animation.id)
+Video.create(title: "South Park", description: "Four kids struggle with the pains of growing up", small_cover_url: "/tmp/south_park.jpg", large_cover_url: "/tmp/south_park_large.jpg", category_id: comedy.id)
