fd41605350635d978a07a54b8f08e4b1e23358f9
Robin Dickson <robindickson@gmail.com>
2015-09-03 15:11:34 +0200

    Simplified code

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -2,7 +2,6 @@ class CategoriesController < ApplicationController
 
   def show
     @category = Category.find(params[:id])
-    @category_videos = Video.where(category_id: @category.id)
   end
 
 end
