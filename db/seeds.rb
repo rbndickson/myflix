329da9bdc1f19119f9dda3382ace6a465e875c93
Robin Dickson <robindickson@gmail.com>
2015-09-03 10:56:47 +0200

    Added videos and categories

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,7 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+Video.create(title: "Avatar - The Last Airbender", description: "The Avatar must master all 4 elements on his quest to save the world", small_cover_url:"avatar.jpg", large_cover_url: "avatar_large.jpg")
+Video.create(title: "The Office", description: "Comedy about British office life", small_cover_url:"office.jpg", large_cover_url: "office_large.jpg")
+Video.create(title: "The Amazing Race", description: "Contestants race around the world completing various challenges", small_cover_url:"amazing_race.jpg", large_cover_url: "amazing_race_large.jpg")
