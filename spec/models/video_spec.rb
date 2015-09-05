3d7ea2aa4defabf0f2010c9c5100a185a01ec390
Robin Dickson <robindickson@gmail.com>
2015-09-05 10:40:02 +0200

    Updated spec to use shoulda matchers

--- spec/models/video_spec.rb
+++ spec/models/video_spec.rb
@@ -1,24 +1,7 @@
 require 'spec_helper'
 
 describe Video do
-  it "saves itself" do
-    video = Video.new(
-      title: "The Amazing Race",
-      description: "Contestants race around the world"
-    )
-    video.save
-    expect(Video.first).to eq(video)
-  end
-end
-
-describe Video do
-  it "belongs to category" do
-    reality = Category.create(name: "Reality")
-    amazing_race = Video.create(
-      title: "The Amazing Race",
-      description: "Contestants race around the world",
-      category: reality
-    )
-    expect(amazing_race.category).to eq(reality)
-  end
+  it { should belong_to(:category) }
+  it { should validate_presence_of(:title) }
+  it { should validate_presence_of(:description) }
 end
