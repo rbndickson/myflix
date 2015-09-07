c6a1538d35d51f02d8fa8fcc7bcfaece6fbf3bfd
Robin Dickson <robindickson@gmail.com>
2015-09-07 10:34:16 +0200

    Updated spec naming

--- spec/models/video_spec.rb
+++ spec/models/video_spec.rb
@@ -5,7 +5,7 @@ describe Video do
   it { should validate_presence_of(:title) }
   it { should validate_presence_of(:description) }
 
-  describe "#search_by_title" do
+  describe ".search_by_title" do
 
     it "returns an empty array if there are no matches" do
       superman = Video.create(title: "Superman", description: "Flying hero!")
