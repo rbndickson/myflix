cfc2c19c7fe228500be0ad6196865de49fc7ed8e
Robin Dickson <robindickson@gmail.com>
2015-09-08 23:20:01 +0200

    Added video reviews

--- spec/controllers/videos_controller_spec.rb
+++ spec/controllers/videos_controller_spec.rb
@@ -1,24 +1,29 @@
 require 'spec_helper'
 
 describe VideosController do
-
   describe "GET show" do
-
     it "assigns @video for authenticated users" do
       session[:user_id] = Fabricate(:user).id
       video = Fabricate(:video)
       get :show, id: video.id
-
       expect(assigns(:video)).to eq(video)
+    end
+
+    it "assigns @reviews for authenticated users" do
+      session[:user_id] = Fabricate(:user).id
+      video = Fabricate(:video)
+      review1 = Fabricate(:review, video: video)
+      review2 = Fabricate(:review, video: video)
+      get :show, id: video.id
+      # matches against the array regardless of order
+      expect(assigns(:reviews)).to match_array([review1, review2])
     end
 
     it "redirects to sign in page for unauthenticated users" do
       video = Fabricate(:video)
       get :show, id: video.id
-
       expect(response).to redirect_to (sign_in_path)
     end
-
   end
 
   describe "GET search" do
@@ -28,8 +33,8 @@ describe VideosController do
       before do
         session[:user_id] = Fabricate(:user).id
         get :search, query: video.title
-
       end
+
       it "assigns @search_term" do
         expect(assigns(:query)).to eq(video.title)
       end
@@ -41,7 +46,6 @@ describe VideosController do
 
     it "redirects to the sign in page for unauthenticated users" do
       get :search, query: video.title
-
       expect(response).to redirect_to(sign_in_path)
     end
   end
