0fa4d6f93daf21833a7e53af0b89b719f81bc0ff
Robin Dickson <robindickson@gmail.com>
2015-09-13 17:59:17 +0200

    Improved queue items controller spec

--- spec/controllers/queue_items_controller_spec.rb
+++ spec/controllers/queue_items_controller_spec.rb
@@ -1,14 +1,17 @@
 require 'spec_helper'
 
 describe QueueItemsController do
+  let(:alice) { Fabricate(:user, id: 1) }
+  let(:superman) { Fabricate(:video, title: "Superman") }
+  let(:batman) { Fabricate(:video, title: "Batman") }
+
   describe 'GET index' do
     it "assigns @queue_items to the queue items of the current user" do
-      alice = Fabricate(:user)
       session[:user_id] = alice.id
-      queue_item1 = Fabricate(:queue_item, video_id: 1, user: alice )
-      queue_item2 = Fabricate(:queue_item, video_id: 2, user: alice )
+      superman_queue_item = Fabricate(:queue_item, video: superman, user: alice)
+      batman_queue_item = Fabricate(:queue_item, video: batman, user: alice)
       get :index
-      expect(assigns(:queue_items)).to match_array([queue_item1, queue_item2])
+      expect(assigns(:queue_items)).to match_array([superman_queue_item, batman_queue_item])
     end
 
     it "redirects to the login page for unauthenticated users" do
@@ -19,8 +22,6 @@ describe QueueItemsController do
 
   describe 'POST create' do
     context "user is authenticated" do
-      let(:alice) { Fabricate(:user) }
-      let(:superman) { Fabricate(:video) }
       before { session[:user_id] = alice.id }
 
       it "redirects to the my queue page" do
@@ -33,7 +34,7 @@ describe QueueItemsController do
         expect(QueueItem.count).to eq(1)
       end
 
-      it "saves a queue item assiciated with the video" do
+      it "saves a queue item associated with the video" do
         post :create, video_id: superman.id
         expect(QueueItem.first.video).to eq(superman)
       end
@@ -44,33 +45,28 @@ describe QueueItemsController do
       end
 
       it "sets the list order position" do
-        batman = Fabricate(:video)
-        spiderman = Fabricate(:video)
-        Fabricate(:queue_item, video_id: batman.id, user_id: alice.id, position: 1)
-        post :create, video_id: spiderman.id
-        spiderman_queue_item = QueueItem.where(video_id: spiderman.id, user_id: alice.id).first
-        expect(spiderman_queue_item.position).to eq(2)
+        Fabricate(:queue_item, video: superman, user: alice, position: 1)
+        post :create, video_id: batman.id
+        batman_queue_item = QueueItem.where(video: batman, user: alice).first
+        expect(batman_queue_item.position).to eq(2)
       end
 
       it "does not add the same video twice" do
-        Fabricate(:queue_item, video_id: superman.id, user_id: alice.id)
+        Fabricate(:queue_item, video: superman, user: alice)
         post :create, video_id: superman.id
-
         expect(QueueItem.count).to eq(1)
       end
     end
 
     context "user is not authenticated" do
       it "redirects to the sign in page if user is not authenticated" do
-        post :create, video_id: 3
+        post :create, video_id: superman.id
         expect(response).to redirect_to(sign_in_path)
       end
     end
 
     describe 'DELETE destroy' do
-      let(:alice) { Fabricate(:user, id: 1) }
-      let(:superman) { Fabricate(:video) }
-      let(:queue_item) { Fabricate(:queue_item, video_id: superman.id, user_id: alice.id, position: 1) }
+      let(:queue_item) { Fabricate(:queue_item, video: superman, user: alice, position: 1) }
 
       it "redirects to the my queue page" do
         session[:user_id] = alice.id
@@ -92,7 +88,7 @@ describe QueueItemsController do
       end
 
       it "redirect to the sign in page for unauthenticated users" do
-        delete :destroy, id: 42
+        delete :destroy, id: queue_item.id
         expect(response).to redirect_to(sign_in_path)
       end
     end
