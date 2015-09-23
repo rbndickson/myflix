f8b4d93d8557d5a962269cb1e9a915809f27d100
Robin Dickson <robindickson@gmail.com>
2015-09-23 13:33:11 +0200

    Refactored videos controller spec

--- spec/controllers/videos_controller_spec.rb
+++ spec/controllers/videos_controller_spec.rb
@@ -2,29 +2,25 @@ require 'spec_helper'
 
 describe VideosController do
   describe "GET show" do
+    let(:video) { Fabricate(:video) }
+
+    it_behaves_like "users must be signed in" do
+      let(:action) { get :show, id: video.id }
+    end
+
     context "with authenticated users" do
+      before { set_current_user }
+
       it "assigns @video" do
-        session[:user_id] = Fabricate(:user).id
-        video = Fabricate(:video)
         get :show, id: video.id
         expect(assigns(:video)).to eq(video)
       end
 
       it "assigns @reviews" do
-        session[:user_id] = Fabricate(:user).id
-        video = Fabricate(:video)
         review1 = Fabricate(:review, video: video)
         review2 = Fabricate(:review, video: video)
         get :show, id: video.id
         expect(assigns(:reviews)).to match_array([review1, review2])
-      end
-    end
-
-    context "with unauthenticated users" do
-      it "redirects to sign in page" do
-        video = Fabricate(:video)
-        get :show, id: video.id
-        expect(response).to redirect_to (sign_in_path)
       end
     end
   end
@@ -32,9 +28,13 @@ describe VideosController do
   describe "GET search" do
     let (:video) { Fabricate(:video) }
 
+    it_behaves_like "users must be signed in" do
+      let(:action) { get :search, query: video.title }
+    end
+
     context "with authenticated users" do
       before do
-        session[:user_id] = Fabricate(:user).id
+        set_current_user
         get :search, query: video.title
       end
 
@@ -44,12 +44,6 @@ describe VideosController do
 
       it "assigns @results" do
         expect(assigns(:results)).to eq([video])
-      end
-    end
-
-    context "with unauthenticated users" do
-      it_behaves_like "users must be signed in" do
-        let(:action) { get :search, query: video.title }
       end
     end
   end
