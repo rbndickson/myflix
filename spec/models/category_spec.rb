48c757a4e52b7c51823dd80a4b755a12c551b46d
Robin Dickson <robindickson@gmail.com>
2015-09-23 15:15:54 +0200

    Refactored category spec

--- spec/models/category_spec.rb
+++ spec/models/category_spec.rb
@@ -5,43 +5,33 @@ describe Category do
   it { should validate_presence_of(:name) }
 
   describe '#recent_videos' do
-    it 'returns an empty array if there are no videos' do
-      hero_movies = Category.create(name:"Hero Movies")
+    let(:hero_movies) { Category.create(name:"Hero Movies") }
 
+    it 'returns an empty array if a category has no videos' do
       expect(hero_movies.recent_videos).to eq([])
     end
 
     it 'returns the videos in reverse chronological order' do
-      hero_movies = Category.create(name:"Hero Movies")
-      superman = Video.create(title: "Superman", description: "Flying hero!", category: hero_movies)
-      spiderman = Video.create(title: "Spiderman", description: "Spider hero!", category: hero_movies)
-      batman = Video.create(title: "Batman", description: "Bat hero!", category: hero_movies)
-
+      superman = Fabricate(:video, category: hero_movies)
+      spiderman = Fabricate(:video, category: hero_movies)
+      batman = Fabricate(:video, category: hero_movies)
       expect(hero_movies.recent_videos).to eq([batman, spiderman, superman])
     end
 
     it 'returns all videos if there are less than 6 videos' do
-      hero_movies = Category.create(name:"Hero Movies")
-      superman = Video.create(title: "Superman", description: "Flying hero!", category: hero_movies)
-      spiderman = Video.create(title: "Spiderman", description: "Spider hero!", category: hero_movies)
-      batman = Video.create(title: "Batman", description: "Bat hero!", category: hero_movies)
-
+      3.times { Fabricate(:video, category: hero_movies) }
       expect(hero_movies.recent_videos.count).to eq(3)
     end
 
     it 'returns six videos if there are 6 or more videos' do
-      hero_movies = Category.create(name:"Hero Movies")
-      10.times { Video.create(title: "Somethingman", description: "Something hero!", category: hero_movies) }
-
+      10.times { Fabricate(:video, category: hero_movies) }
       expect(hero_movies.recent_videos.count).to eq(6)
     end
 
     it 'returns the most recent 6 videos' do
-      hero_movies = Category.create(name:"Hero Movies")
-      10.times { Video.create(title: "Somethingman", description: "Something hero!", category: hero_movies) }
-      superman = Video.create(title: "Superman", description: "Flying hero!", category: hero_movies, created_at: 1.day.ago)
-
-      expect(hero_movies.recent_videos).not_to include(superman)
+      10.times { Fabricate(:video, category: hero_movies) }
+      batman = Fabricate(:video, category: hero_movies, created_at: 1.day.ago)
+      expect(hero_movies.recent_videos).not_to include(batman)
     end
   end
 end
