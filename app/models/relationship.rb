c5eb5e5449ba1569d0ed06c4ebe2239d378c12a9
Robin Dickson <robindickson@gmail.com>
2015-10-21 15:08:47 +0100

    Remove unnecessary validations

--- app/models/relationship.rb
+++ app/models/relationship.rb
@@ -1,7 +1,5 @@
 class Relationship < ActiveRecord::Base
   belongs_to :follower, class_name: 'User'
   belongs_to :leader, class_name: 'User'
-  validates :follower_id, presence: true
-  validates :leader_id, presence: true
   validates_uniqueness_of :follower_id, scope: [:leader_id], on: :create
 end
--- spec/models/relationship_spec.rb
+++ spec/models/relationship_spec.rb
@@ -3,6 +3,4 @@ require 'spec_helper'
 describe Relationship do
   it { should belong_to(:follower).class_name('User') }
   it { should belong_to(:leader).class_name('User') }
-  it { should validate_presence_of(:follower_id) }
-  it { should validate_presence_of(:leader_id) }
 end
