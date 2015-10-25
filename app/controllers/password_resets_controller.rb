1ac257da9efb78c8e7b1368800e150d4f141d064
Robin Dickson <robindickson@gmail.com>
2015-10-25 21:58:09 +0000

    Updates from PR comment

--- app/controllers/password_resets_controller.rb
+++ app/controllers/password_resets_controller.rb
@@ -1,6 +1,6 @@
 class PasswordResetsController < ApplicationController
   def show
-    user = User.where(token: params[:id]).first
+    user = User.find_by(token: params[:id])
     if user
       @token = user.token
     else
@@ -9,7 +9,7 @@ class PasswordResetsController < ApplicationController
   end
 
   def create
-    user = User.where(token: params[:token]).first
+    user = User.find_by(token: params[:token])
     if user
       user.password = params[:password]
       user.generate_token
