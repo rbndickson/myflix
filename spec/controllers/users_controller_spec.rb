2a803a65dc281bf422967ef06959efc3437ce583
Robin Dickson <robindickson@gmail.com>
2015-10-28 12:46:08 +0000

    Simplify spec expectation

--- spec/controllers/users_controller_spec.rb
+++ spec/controllers/users_controller_spec.rb
@@ -4,8 +4,7 @@ describe UsersController do
   describe "GET new" do
     it "assigns @user" do
       get :new
-      expect(assigns(:user)).to be_new_record
-      expect(assigns(:user)).to be_instance_of(User)
+      expect(assigns(:user)).to be_a_new(User)
     end
   end
 
