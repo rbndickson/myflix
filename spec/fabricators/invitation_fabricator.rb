66b06c7455f0df30af8d03378c481d60c735d4dd
Robin Dickson <robindickson@gmail.com>
2015-11-03 12:06:57 +0000

    Fix indentation

--- spec/fabricators/invitation_fabricator.rb
+++ spec/fabricators/invitation_fabricator.rb
@@ -1,5 +1,5 @@
- Fabricator(:invitation) do
+Fabricator(:invitation) do
   recipient_name { Faker::Name.name }
   recipient_email { Faker::Internet.email }
   message { Faker::Lorem.paragraph }
- end
+end
