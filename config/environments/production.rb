863b4d80721a057d95f8192f0c3c132d11979171
Robin Dickson <robindickson@gmail.com>
2015-11-03 15:34:17 +0000

    Update production mailer config

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -22,6 +22,7 @@ Myflix::Application.configure do
 
   config.action_mailer.perform_deliveries = true
 
+  config.action_mailer.default_url_options = { host: 'rbn-myflix.herokuapp.com' }
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.smtp_settings = {
   :port           => ENV['MAILGUN_SMTP_PORT'],
