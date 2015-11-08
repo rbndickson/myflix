2fe3c02e70bc6a37da54d5d51938a8ada0f0ab90
Robin Dickson <robindickson@gmail.com>
2015-11-08 16:24:38 +0000

    Add admin column to user table

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20151029100708) do
+ActiveRecord::Schema.define(version: 20151108161959) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -67,6 +67,7 @@ ActiveRecord::Schema.define(version: 20151029100708) do
     t.datetime "created_at"
     t.datetime "updated_at"
     t.string   "token"
+    t.boolean  "admin"
   end
 
   create_table "videos", force: true do |t|
