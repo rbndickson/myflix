6216d075653ec719165769335855076f542dc9b8
Robin Dickson <robindickson@gmail.com>
2015-11-10 16:18:44 +0000

    Add image uploading when adding video in development environment

--- app/controllers/admin/videos_controller.rb
+++ app/controllers/admin/videos_controller.rb
@@ -6,6 +6,24 @@ class Admin::VideosController < ApplicationController
     @video = Video.new
   end
 
+  def create
+    @video = Video.new(video_params)
+
+    if @video.save
+      redirect_to new_admin_video_path
+      flash[:success] = "You have added #{@video.title}"
+    else
+      flash[:danger] = "Video has not been added - Please check the errors"
+      render :new
+    end
+  end
+
+  private
+
+  def video_params
+    params.require(:video).permit(:title, :category_id, :description, :small_cover, :large_cover)
+  end
+
   def require_admin
     unless current_user.admin?
       flash[:danger] = "You do not have access"
--- spec/support/shared_examples.rb
+++ spec/support/shared_examples.rb
@@ -6,6 +6,14 @@ shared_examples "users must be signed in" do
   end
 end
 
+shared_examples "admin is required" do
+  it "redirects a regular user to the home page" do
+    set_current_user
+    action
+    expect(response).to redirect_to(home_path)
+  end
+end
+
 shared_examples "a random token is generated" do
   it "a random token is generated on creation" do
     expect(object.token).to be_present
