4d039e23196ff812e41b10b924ad40bbb4a85acc
Robin Dickson <robindickson@gmail.com>
2015-11-16 15:40:26 +0000

    Add Stripe API wrapper

---
+++ app/models/stripe_wrapper.rb
@@ -0,0 +1,12 @@
+module StripeWrapper
+  class Charge
+    def self.create(options={})
+      Stripe::Charge.create(
+        amount: options[:amount],
+        currency: "usd",
+        source: options[:source],
+        description: options[:description]
+      )
+    end
+  end
+end
--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -21,6 +21,12 @@ Dir[Rails.root.join("spec/support/**/*.rb")].each { |f| require f }
 # If you are not using ActiveRecord, you can remove this line.
 ActiveRecord::Migration.maintain_test_schema!
 
+VCR.configure do |c|
+  c.cassette_library_dir = 'spec/cassettes'
+  c.hook_into :webmock
+  c.configure_rspec_metadata!
+end
+
 RSpec.configure do |config|
   # ## Mock Framework
   #
@@ -63,4 +69,7 @@ RSpec.configure do |config|
   # The different available types are documented in the features, such as in
   # https://relishapp.com/rspec/rspec-rails/v/3-0/docs
   config.infer_spec_type_from_file_location!
+  
+  # so we can use :vcr rather than :vcr => true;
+  config.treat_symbols_as_metadata_keys_with_true_values = true
 end
