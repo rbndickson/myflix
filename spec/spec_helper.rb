e56837c15e65730fe5f8bbfaf65a8de3e61b96cb
Robin Dickson <robindickson@gmail.com>
2015-11-16 20:33:21 +0000

    Add config for database cleaner

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -45,7 +45,7 @@ RSpec.configure do |config|
   # If you're not using ActiveRecord, or you'd prefer not to run each of your
   # examples within a transaction, remove the following line or assign false
   # instead of true.
-  config.use_transactional_fixtures = true
+  config.use_transactional_fixtures = false
 
   # If true, the base class of anonymous controllers will be inferred
   # automatically. This will be the default behavior in future versions of
@@ -75,4 +75,24 @@ RSpec.configure do |config|
 
   # so we can use :vcr rather than :vcr => true;
   config.treat_symbols_as_metadata_keys_with_true_values = true
+
+  config.before(:suite) do
+    DatabaseCleaner.clean_with(:truncation)
+  end
+
+  config.before(:each) do
+    DatabaseCleaner.strategy = :transaction
+  end
+
+  config.before(:each, :js => true) do
+    DatabaseCleaner.strategy = :truncation
+  end
+
+  config.before(:each) do
+    DatabaseCleaner.start
+  end
+
+  config.after(:each) do
+    DatabaseCleaner.clean
+  end
 end
