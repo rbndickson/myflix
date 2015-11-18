071ff57a9ab46564902d6dd17ae7832f93412473
Robin Dickson <robindickson@gmail.com>
2015-11-18 17:49:15 +0000

    Update stripe wrapper model to handle declined credit cards

--- spec/models/stripe_wrapper_spec.rb
+++ spec/models/stripe_wrapper_spec.rb
@@ -20,8 +20,47 @@ describe StripeWrapper do
           :description => "Valid example charge"
         )
 
-        expect(response.amount).to eq(999)
-        expect(response.currency).to eq("usd")
+        expect(response).to be_successful
+      end
+
+      it "makes a card declined charge", :vcr do
+        Stripe.api_key = ENV['STRIPE_SECRET_KEY']
+        token = Stripe::Token.create(
+          :card => {
+            :number => "4000000000000002",
+            :exp_month => 6,
+            :exp_year => 2018,
+            :cvc => 314
+          }
+        ).id
+
+        response = StripeWrapper::Charge.create(
+          :amount => 999,
+          :source => token,
+          :description => "Invalid example charge"
+        )
+
+        expect(response).not_to be_successful
+      end
+
+      it "returns the error message for declined charges", :vcr do
+        Stripe.api_key = ENV['STRIPE_SECRET_KEY']
+        token = Stripe::Token.create(
+          :card => {
+            :number => "4000000000000002",
+            :exp_month => 6,
+            :exp_year => 2018,
+            :cvc => 314
+          }
+        ).id
+
+        response = StripeWrapper::Charge.create(
+          :amount => 999,
+          :source => token,
+          :description => "Invalid example charge"
+        )
+
+        expect(response.error_message).to eq("Your card was declined.")
       end
     end
   end
