bd3f5da796c7fa6cabb34c50dab3ef080dea8d47
Robin Dickson <robindickson@gmail.com>
2015-11-23 22:10:42 +0000

    Move video display average rating method to video decorator

--- app/controllers/videos_controller.rb
+++ app/controllers/videos_controller.rb
@@ -6,7 +6,7 @@ class VideosController < ApplicationController
   end
 
   def show
-    @video = Video.find(params[:id])
+    @video = Video.find(params[:id]).decorate
     @reviews = @video.reviews
   end
 
