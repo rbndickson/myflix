e48e0745ed0e048bf489918e2bd23d0f9605b2e7
Robin Dickson <robindickson@gmail.com>
2015-12-02 12:12:51 +0000

    Add customer token to users table

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20151202115001) do
+ActiveRecord::Schema.define(version: 20151202120851) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -74,6 +74,7 @@ ActiveRecord::Schema.define(version: 20151202115001) do
     t.datetime "updated_at"
     t.string   "token"
     t.boolean  "admin"
+    t.string   "customer_token"
   end
 
   create_table "videos", force: true do |t|
