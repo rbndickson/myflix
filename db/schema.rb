5ff123cfeabc52f9615dc5bd11b2f574d7d89fd7
Robin Dickson <robindickson@gmail.com>
2015-12-03 15:44:25 +0000

    Add 'active' column to user table

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20151202120851) do
+ActiveRecord::Schema.define(version: 20151203142847) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -75,6 +75,7 @@ ActiveRecord::Schema.define(version: 20151202120851) do
     t.string   "token"
     t.boolean  "admin"
     t.string   "customer_token"
+    t.boolean  "active",          default: true
   end
 
   create_table "videos", force: true do |t|
