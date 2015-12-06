97a591c59e0daeabad119d03a5220f707501b0bb
Robin Dickson <robindickson@gmail.com>
2015-12-06 19:48:27 +0000

    Update payment record creation to bang method so an error will be raised in the event of failure

--- config/initializers/stripe.rb
+++ config/initializers/stripe.rb
@@ -3,7 +3,7 @@ Stripe.api_key = ENV['STRIPE_SECRET_KEY']
 StripeEvent.configure do
   subscribe 'charge.succeeded' do |event|
     user = User.find_by(customer_token: event.data.object.customer)
-    Payment.create(
+    Payment.create!(
       user: user,
       amount: event.data.object.amount,
       reference_id: event.data.object.id
